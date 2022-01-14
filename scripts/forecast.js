c17e5ffd465a9ec26f38033f6988276a0a80e6ed
Mensur Veladžić <mensur_mero@hotmail.com>
2022-01-14 17:07:28 +0100

    Weather App

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = 'f7KGlFEp1QaOyp3YsZ6nQBCZeMeHpeHz';
+
+// GET WEATHER INFORMATIOn
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// GET CITY INFORMATION
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
